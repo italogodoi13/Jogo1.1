f1b484b16a71838f8972719dcb24ccc3d0405080
italogodoi13 <66567134+italogodoi13@users.noreply.github.com>
2020-06-07 19:40:38 -0300

    Add files via upload

--- sprite.js
+++ sprite.js
@@ -6,8 +6,16 @@ this.altura = altura;
 
 this.desenha = function(xCanvas, yCanvas){
     ctx.drawImage(img, this.x,this.y,this.largura,this.altura, xCanvas, yCanvas, this.largura,this.altura);
+    
+},
+this.desenhaCenario = function(xCanvas,yCanvas){
+    ctx.drawImage(cenario, this.x,this.y,this.largura,this.altura, xCanvas, yCanvas, this.largura,this.altura);
 }
 
 }
 
-var imgBloco = new sprite(243,408, 70, 100)
+var imgBloco = new sprite(14,257, 130, 170)
+var imgObs = new sprite(7,485,130,170)
+var imgBalaBloco = new sprite(314,15,50,20)
+
+var imgCenario = new sprite(41,21,600,600)
